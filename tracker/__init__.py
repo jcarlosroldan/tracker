1283ff22cd38e7ac29a3b4b96fe32478a4972b2b
Juan C. Roldán <juancarlos@sevilla.es>
2021-09-10 12:16:05 +0200

    Typed methods, removed unused imports and whitespace

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,16 +1,17 @@
 from atexit import register
+from typing import Callable, Tuple
 from setproctitle import setproctitle
 from threading import Thread
 from time import sleep, time
 from tracker.idle import idle_start
 from tracker.keyboard import keyboard_start
-from tracker.lock import lock_start, lock_start
+from tracker.lock import lock_start
 from tracker.mouse import mouse_start
 from tracker.server import server_start
 from tracker.utils import cfg, error, log
 from tracker.window import window_start
 
-def run():
+def run() -> None:
 	try:
 		setproctitle(cfg('proctitle'))
 		cfg('running', True)
@@ -23,19 +24,17 @@ def run():
 			mouse_start()  # XXX add joystick tracking and periodic snapshots
 		)
 		log('begin')
-		# track_window is ran in the main thread due to a Windows 10 bug with running GetWindowText in separate threads
 		[t.start() for t in threads]
-		loop(*window_start())
+		loop(*window_start())  # GetWindowText requires being in the main thread due to W10 bug
 		[t.join() for t in threads]
 	except:
 		error('TRACKER INIT')
 
-def loop(target, throttle=0):
+def loop(target: Tuple[Callable, float], throttle=0) -> None:
 	try:
 		while cfg('running'):
-			delay = time()
+			now = time()
 			target()
-			delay = time() - delay
-			if delay < throttle: sleep(throttle - delay)
+			sleep(max(0, now + throttle - time()))
 	except:
 		error(target.__name__.upper())
